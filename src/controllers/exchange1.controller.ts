adeb1cd435b8f1014d3489c466a061aa2795bda0
adityam789 <adityamahesh@umass.edu>
2022-01-01 15:31:44 +0530

    dets

--- src/controllers/exchange1.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export default class ExchangeController{
-    public buy(req: Request, res: Response, next: NextFunction){
-        const amount = req.body.amount;
-        const {buy, sell} = req.body.coinpair.split('-');
-        
-    }
-}
