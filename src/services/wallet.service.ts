32ce52b8a6fc0f7d863f497e3b5af9431147b5c0
adityam789 <adityamahesh@umass.edu>
2022-01-01 15:31:08 +0530

    Homer

--- src/services/wallet.service.ts
+++ src/services/wallet.service.ts
@@ -10,7 +10,6 @@ export default class WalletService {
     wallet.updated_at = new Date();
     await wallet.save();
   }
-
   public async createWallet(
     user_id: string,
     currency: string,
@@ -23,8 +22,8 @@ export default class WalletService {
     });
 
     await wallet.save();
+    return wallet._id;
   }
-
   public async withdrawFund(wallet_id: string, amount: number) {
     const wallet = await WalletModel.findById(wallet_id);
     if (!wallet) {
@@ -44,5 +43,21 @@ export default class WalletService {
   public async getbyCoin(user_id: string, currency: string) {
     const wallets = await WalletModel.find({ user_id, currency });
     return wallets;
+  }
+  public async exchangeBetweenWallet(wallet_id_from: string, wallet_id_to: string, amount: number) {
+    const wallet_from = await WalletModel.findById(wallet_id_from);
+    const wallet_to = await WalletModel.findById(wallet_id_to);
+    if (!wallet_from || !wallet_to) {
+      throw new Error("Wallet not found");
+    }
+    if (wallet_from.balance < amount) {
+      throw new Error("Insufficient funds");
+    }
+    wallet_from.balance -= amount;
+    wallet_to.balance += amount;
+    wallet_from.updated_at = new Date();
+    wallet_to.updated_at = new Date();
+    await wallet_from.save();
+    await wallet_to.save();
   }
 }
